08971e51faef4d666b3c7064d605053049055197
arnofrxdd <bazzukagaming317@gmail.com>
2026-08-01 01:18:51 +0530

    fixes

--- api/services/reports/daily_report.py
+++ api/services/reports/daily_report.py
@@ -48,7 +48,10 @@ class DailyReportService:
             1
             for run in runs
             if run["gathered_context"]
-            and run["gathered_context"].get("mapped_call_disposition") == "XFER"
+            and str(
+                run["gathered_context"].get("mapped_call_disposition", "")
+            ).lower()
+            in ("xfer", "transfer_call")
         )
 
         # Calculate disposition distribution
